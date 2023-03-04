d0fee0eeb816de65f879aa5540c5dc3d9cd4a01c
SandeepKushwaha <sandeepkush327@gmail.com>
2023-03-04 18:44:55 +0530

    patterns in cpp

---
+++ Introduction_to_programming/6_patterns/assignments/q.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// ==================================================
+// Complete the code for following pattern: 
+//  if n = 3
+//
+//  123321
+//  12**21
+//  1****1
+//
+// ==================================================
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // code here.
+
+    return 0;
+}
---
+++ Introduction_to_programming/6_patterns/assignments/r.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// ==================================================
+// Complete the code for following pattern: 
+//  if n = 4
+//
+//  ABCDDCBA
+//   BCDDCB
+//    CDDC
+//     DD
+// 
+// constraints: 1 <= n <= 26 
+//
+// ==================================================
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // code here.
+
+    return 0;
+}
